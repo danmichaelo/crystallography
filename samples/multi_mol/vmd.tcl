e8d06da02dc964dd101bebff09286cd2fc69904b
Dan Michael Olsen Heggø <danmichaelo@gmail.com>
2011-12-06 19:00:57 +0100

    Updated samples

--- samples/multi_mol/vmd.tcl
+++ samples/multi_mol/vmd.tcl
@@ -1,22 +1,26 @@
 
 display rendermode GLSL
 
+package require pbctools
+
 # Load crystallography package if it hasn't been loaded already 
 if { [catch {package present crystallography_gui}] } {
     vmd_install_extension crystallography_gui cryst_tk "Crystallography"
 }
 
-crystal_debug on
+cryst_debug on
 cryst_tk
 
-# Load a molecule with unit cell data
-mol new {1CRN.pdb} type {pdb} waitfor -1
+# Load a molecule with orthorhombic unit cell:
+set mol1 [mol new {1BUW.pdb} type {pdb} waitfor -1]
+pbc box -molid $mol1
 
-# one with a hexagonal cell
-mol new {quartz.POSCAR} type {POSCAR} waitfor -1
+# ... and one with a hexagonal cell:
+set mol2 [mol new {quartz.POSCAR} type {POSCAR} waitfor -1]
+pbc box -molid $mol2
 
-# and one without
-mol new {ice.pdb} type {pdb} waitfor -1
+# ... and one without unit cell information:
+set mol3 [mol new {ice.pdb} type {pdb} waitfor -1]
 
-#crystal_axes on
+#cryst_axes on
 
